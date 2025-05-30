d15f780f2fec520d7fdb2c3e3a4c969dd065ebdd
Frank2006x <frank2006x@gmail.com>
2025-05-30 22:55:33 +0530

    home layout:

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center">
+          <input type="text" placeholder="Search" className="input" />
+        </div>
+
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
